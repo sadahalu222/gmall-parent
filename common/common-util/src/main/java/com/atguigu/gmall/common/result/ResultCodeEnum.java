48be9a2ebd1f9bdf2ff64af86b9d66e38728c299
朱重宁 <2446973061@qq.com>
2022-10-03 19:27:04 +0800

    day13 单点登入退出

--- common/common-util/src/main/java/com/atguigu/gmall/common/result/ResultCodeEnum.java
+++ common/common-util/src/main/java/com/atguigu/gmall/common/result/ResultCodeEnum.java
@@ -15,6 +15,7 @@ public enum ResultCodeEnum {
 
     PAY_RUN(205, "支付中"),
 
+    LOGIN_ERROR(2081,"账号密码错误"),
     LOGIN_AUTH(208, "未登陆"),
     PERMISSION(209, "没有权限"),
     SECKILL_NO_START(210, "秒杀还没开始"),
